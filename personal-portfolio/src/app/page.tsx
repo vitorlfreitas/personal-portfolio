41b0b08130171d74072cd6545af0f2f4d649d53d
vitorlfreitas <vitor.freitas@student.griffith.ie>
2024-10-03 12:04:38 +0100

    Adds New Section: Latest Projects Developed

--- personal-portfolio/src/app/page.tsx
+++ personal-portfolio/src/app/page.tsx
@@ -7,6 +7,19 @@ import { useEffect, useState } from "react";
 import { BsFillMoonStarsFill, BsSunFill } from "react-icons/bs";
 import { FaGithubSquare, FaInstagramSquare, FaLinkedin } from "react-icons/fa";
 import skills from "../data/skills";
+import { FaExternalLinkAlt } from "react-icons/fa";
+import "../../styles/styles.css";
+
+import {
+    Button,
+    Card,
+    CardBody,
+    CardFooter,
+    CardHeader,
+    Heading,
+    Text,
+} from "@chakra-ui/react";
+import { p } from "framer-motion/client";
 
 export default function Home() {
     const [darkMode, setDarkMode] = useState(false);
@@ -57,8 +70,8 @@ export default function Home() {
     };
 
     return (
-        <div className={"w-screen overflow-x-hidden"}>
-            <main className="bg-white dark:bg-gray-800">
+        <div className="w-screen overflow-x-hidden bg-gray-50 dark:bg-gray-800">
+            <main>
                 <section>
                     <nav className="w-full pl-4 pr-8 md:pr-12 py-4 mb-12 flex justify-between md:mb-2">
                         <a
@@ -196,8 +209,40 @@ export default function Home() {
                     </div>
                     <div className="mt-5">
                         <h2 className="mt-5 text-2xl text-gray-800 font-semibold">
-                            Projects
+                            Checkout My Latest Project
                         </h2>
+                        <div className="mx-auto my-6 shadow-md shadow-gray-700 rounded-xl md:flex">
+                            <img
+                                className="rounded-t-xl md:rounded-l-xl md:rounded-r-none md:image-square"
+                                src="../assets/bartender-website-cover.webp"
+                                alt="Image of the The Complete Bartender Guide"
+                            />
+                            <div className="p-4 rounded-b-xl bg-gray-50 dark:bg-gray-800 md:py-2 md:px-4 md:flex md:flex-col md:justify-between">
+                                <div>
+                                    <hr className="my-2 sm:hidden md:block" />
+                                    <h3 className="font-semibold text-lg italic dark:text-gray-100 ">
+                                        The Complete Bartender Guide
+                                    </h3>
+                                </div>
+                                {!isMobile && (
+                                    <p className="text-sm py-2 leading-6 text-gray-800">
+                                        A website to guide aspirants of
+                                        bartending to learn the basics steps,
+                                        classic cocktails and techniques which I
+                                        have acquired throughout my life. I have been a Bartender for over 9 years and am an aspirant of web development, so I decided to invest my time to develop a website to share my knowledge while training my website skills
+                                    </p>
+                                )}
+                                <div>
+                                    <a
+                                        className="bg-gradient-to-r from-sky-700 to-teal-500 ml-auto flex gap-2 items-center back w-fit py-1 px-2 rounded my-4 text-gray-100 font-semibold md:my-2"
+                                        href="#"
+                                    >
+                                        <FaExternalLinkAlt /> Visit Website
+                                    </a>
+                                    <hr className="my-2" />
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </section>
             </main>
